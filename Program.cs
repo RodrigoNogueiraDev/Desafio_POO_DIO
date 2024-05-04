39c9bc1b9c0a9b9207ee1c7ecd3d73cc1c4a3f7e
Rodrigo Nogueira <rodrigonogueiram@gmail.com>
2024-05-03 23:12:48 -0300

    alteracao

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia(numero: "61982288581", modelo: "Nokia", imei: "12345678", memoria: 256);
+Console.WriteLine(nokia.Numero);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagran");
+
+Console.WriteLine();
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone(numero: "61985812275", modelo: "Iphone", imei: "55555555", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whats");
